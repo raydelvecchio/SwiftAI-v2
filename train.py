01de8d67bf80db186b0e8d6235a9708921ab64a8
xrayd <72351211+xrayd@users.noreply.github.com>
2022-11-22 01:30:09 -0500

    use_gpu switch implemented

--- train.py
+++ train.py
@@ -7,14 +7,16 @@ import torch
 
 
 class SwiftAITrainer:
-    def __init__(self, train_size=0.8, batch_size=1, learning_rate=1e-3, epochs=3, warmup_steps=1e3, lr_cycles=4):
+    def __init__(self, use_gpu=True, train_size=0.8, batch_size=1, learning_rate=1e-3, epochs=3, warmup_steps=1e3,
+                 lr_cycles=4):
         """
         Initializes the pre-trained GPT2 model from configuration, resizes embedding to include new vocabulary created
         (such as start, pad, newline, UNK, etc), creates training and validation dataloaders for training, defines
         optimizer, and defines scheduler to use to adjust learning rate over time. I use a cosine restarting warmup
         schedule to define how my learning rate changes as we train. To achieve a higher accuracy, we can adjust the
         input hyperparameters until we converge to a better model. If this doesn't work, we can examine docs for
-        our optimizer and scheduler to change default values if need be.
+        our optimizer and scheduler to change default values if need be. Can opt to use GPU for training or stick
+        with CPU usage only with use_gpu parameter.
         Config docs: https://huggingface.co/docs/transformers/main_classes/configuration#transformers.PretrainedConfig.
         Pre-Model docs: https://huggingface.co/docs/transformers/main/en/main_classes/model#transformers.PreTrainedModel.
         Pytorch Module docs: https://pytorch.org/docs/stable/generated/torch.nn.Module.html.
@@ -23,27 +25,30 @@ class SwiftAITrainer:
         Scheduler docs: https://huggingface.co/docs/transformers/main_classes/optimizer_schedules#transformers.SchedulerType.
         GPT2LMHead docs: https://huggingface.co/docs/transformers/model_doc/gpt2#transformers.GPT2LMHeadModel.
         """
-        torch.set_default_tensor_type(torch.cuda.FloatTensor)
-
         print("Creating all training objects and variables...")
         self.epochs = epochs
+        self.use_gpu = use_gpu
 
-        gpu_gen = torch.Generator(device="cuda")
-        cpu_gen = torch.Generator(device="cpu")
+        if use_gpu:
+            torch.set_default_tensor_type(torch.cuda.FloatTensor)
+            generator = torch.Generator(device="cuda")
+        else:
+            generator = torch.Generator(device="cpu")
         lines_dataset, tokenizer = preprocess_get_dataset_and_tokenizer()
         train_data, validation_data = random_split(lines_dataset,
                                                    [math.floor(train_size * len(lines_dataset)),
                                                     len(lines_dataset) - math.floor(train_size * len(lines_dataset))],
-                                                   generator=gpu_gen)
+                                                   generator=generator)
         self.train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, drop_last=True,
-                                       generator=gpu_gen)
+                                       generator=generator)
         self.validation_loader = DataLoader(validation_data, batch_size=batch_size, shuffle=True, drop_last=True,
-                                            generator=gpu_gen)
+                                            generator=generator)
 
         config = GPT2Config.from_pretrained('gpt2')
         self.model = GPT2LMHeadModel(config)
         self.model.resize_token_embeddings(len(tokenizer))
-        self.model.cuda()
+        if use_gpu:
+            self.model.cuda()
 
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=learning_rate)
         self.scheduler = get_cosine_with_hard_restarts_schedule_with_warmup(self.optimizer,
@@ -77,15 +82,17 @@ class SwiftAITrainer:
         for epoch in range(self.epochs):
             print(f'Training Epoch {epoch + 1}...')
             for batch in self.train_loader:
-                # inputs and labels are the same since we're using GPT2LMHead Model, which creates labels from inputs
-                inputs = batch.cuda().long()
-                # inputs = batch.long()
+                if self.use_gpu:
+                    inputs = batch.cuda().long()
+                else:
+                    inputs = batch.long()
 
                 # grads zeroed; Pytorch accumulates them, so we must reset lest gradients be influenced by prev grads
                 self.model.zero_grad()
                 self.optimizer.zero_grad()
 
                 # forward[0] is loss Tensor of next token prediction, forward[1] is logits Tensor
+                # inputs and labels are the same since we're using GPT2LMHead Model, which creates labels from inputs
                 outputs = self.model.forward(inputs, labels=inputs)
                 loss = outputs[0]
                 loss_val = float(loss.item())
