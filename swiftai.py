4a8aa73634993b304a734dc4a738f276b6a1e70c
xrayd <72351211+xrayd@users.noreply.github.com>
2022-11-23 15:24:54 -0500

    better sampling!!

--- swiftai.py
+++ swiftai.py
@@ -29,19 +29,23 @@ class SwiftAI:
 
         self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
-    def make_predictions(self, text_prompt: str, max_length=20) -> str:
+    def make_predictions(self, text_prompt: str, length=20, k=25, p=0.9, temp=1) -> str:
         """
-        Given a text prompt, generate text with our model!
+        Given a text prompt, generate text with our model! Hyperparameters like max length, k, p, and temp can be
+        adjusted to vary the generation of text that we produce. In the future, we could do many samples with a lot of
+        k, p, and temp and compare it to a metric to find the "best" generation.
         Example text generation: https://huggingface.co/blog/how-to-generate
+        Generate function docs: https://huggingface.co/docs/transformers/v4.24.0/en/main_classes/text_generation#transformers.generation_utils.GenerationMixin.generate
         """
         text_prompt = clean_line(text_prompt)
         self.model.eval()
         starting_tokens = self.tokenizer.encode(text_prompt, return_tensors='pt').to(self.device)
-        output = self.model.generate(starting_tokens, max_length=max_length)[0]  # best output is idx 1
+        output = self.model.generate(starting_tokens, do_sample=True, top_k=k, top_p=p, max_length=length,
+                                     temperature=temp)[0]
         return self.tokenizer.decode(output)
 
 
 if __name__ == "__main__":
     swift = SwiftAI('saved_vars/untrained_swiftai_model.pth')
-    lines = swift.make_predictions("I love Antonio because he is")
+    lines = swift.make_predictions("Why is Ray so smart?")
     print(lines)
